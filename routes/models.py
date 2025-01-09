a2ffd3884533e78e5a2c146783380002682a5bec
Alice Mahtani <alice.mahtani@gmail.com>
2025-01-09 13:58:37 +0000

    add route model

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Route(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    date = models.DateField()
+    start_point = models.CharField(max_length=100)
+    end_point = models.CharField(max_length=100)
+    distance = models.FloatField()
+    elevation = models.FloatField()
+    time_taken = models.TimeField()
+    route_img = models.ImageField(upload_to="static/images/")
+    comments = models.TextField()
+   

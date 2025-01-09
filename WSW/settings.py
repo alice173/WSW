ec05bc4a2643bce420840da1da1ca0a3c2736094
Alice Mahtani <alice.mahtani@gmail.com>
2025-01-09 13:43:51 +0000

    add walk model + admin

--- WSW/settings.py
+++ WSW/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-qu=e%r4fj4+0uk7^p@(h)c19$as1lxxu_%^(k731m#%zv3ai9q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
--- walks/models.py
+++ walks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
+
+class Walk(models.Model):
+  title = models.CharField(max_length=100, unique=True)
+  slug = models.SlugField(max_length=100, unique=True)
+  map_img = models.ImageField(upload_to="static/images/")
+  content = models.TextField()
+  status = models.IntegerField(choices=STATUS, default=0)

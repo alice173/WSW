8affcdaa18c6d0362d6077086a9024dd5de7b7b3
Alice Mahtani <alice.mahtani@gmail.com>
2025-01-10 10:22:34 +0000

    add difficulty level model

--- walks/admin.py
+++ walks/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Walk
+from .models import Walk, DifficultyLevel
 # Register your models here.
 admin.site.register(Walk)
+admin.site.register(DifficultyLevel)
--- walks/models.py
+++ walks/models.py
@@ -4,9 +4,18 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
+class DifficultyLevel(models.Model):
+  level = models.CharField(max_length=100)
+  walk = models.ForeignKey(
+        'Walk', on_delete=models.CASCADE, related_name="walk_difficulty_level")
+
+
 class Walk(models.Model):
   title = models.CharField(max_length=100, unique=True)
   slug = models.SlugField(max_length=100, unique=True)
+  difficulty = models.ForeignKey(DifficultyLevel, on_delete=models.CASCADE,
+  related_name="walk_difficulty_level", null=True, blank=True)
+  elevation_gain = models.IntegerField()
   map_img = models.ImageField(upload_to="static/images/")
   content = models.TextField()
   status = models.IntegerField(choices=STATUS, default=0)

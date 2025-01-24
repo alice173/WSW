0d8f471a849c8492ee51862079e82af1a3349ce3
Alice Mahtani <alice.mahtani@gmail.com>
2025-01-24 16:56:31 +0000

    add js to mobile menu button

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+  console.log("hello");
   const navToggle = document.querySelector(".nav-toggle");
   const navList = document.querySelector(".nav__list");
+  const closeButton = document.querySelector(".nav-close");
 
   navToggle.addEventListener("click", function () {
-    navList.classList.toggle("active");
+    navList.classList.add("mobile-open");
+  });
+
+  closeButton.addEventListener("click", function () {
+    navList.classList.remove("mobile-open");
   });
 });

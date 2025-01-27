7f777621518a0c1c8e3eae2830c006d9079202c8
Alice Mahtani <alice.mahtani@gmail.com>
2025-01-27 09:29:38 +0000

    add invalid form

--- routes/test_forms.py
+++ routes/test_forms.py
@@ -15,3 +15,16 @@ class TestRouteForm(TestCase):
             'comments': 'A beautiful walk along the coast.'
         })
         self.assertTrue(route_form.is_valid(), msg=route_form.errors)
+
+    def test_form_is_invalid(self):
+        route_form = RouteForm({
+            'title': '',
+            'start_point': '50.579749, -4.540113',
+            'end_point': '50.694718, -3.954859',
+            'distance': 10.5,
+            'elevation': 200,
+            'date': '2023-10-10',
+            'time_taken': '2',
+            'comments': 'A beautiful walk along the coast.'
+        })
+        self.assertFalse(route_form.is_valid(), msg=route_form.errors)
